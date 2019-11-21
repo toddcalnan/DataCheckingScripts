42fbdc24bd46eaba65c19a014ec19f2e0fa145cb
Todd Calnan <todd.calnan@duke.edu>
2019-11-21 09:34:39 -0500

    Updates to variable names and structure to increase clarity. Added in block comments and line comments to explain what each section is trying to accomplish.

--- batchFileChecker.m
+++ batchFileChecker.m
@@ -1,47 +1,53 @@
+% Script to be used after running all EEG subjects through
+% automatedMaster_Run.m 
+
+% The path should contain all of the analyzed subject files outputted from
+% automatedMaster_Run. Each of these files will be of the form
+% subjectName_maxChanInterp or subjectName_maxAlphaFreqs.
+
+% This script opens up each file in the path, looks for certain criteria to
+% be met ( alpha band peak too high, too many interpolated channels) and
+% flags the subjects that meet those criteria. 
+
+% Someone will need to go through and reanalyze those flagged subjects. 
+
 clear
-%thePath = 'C:\Users\tmc54\Documents\EEG\NARSAD_Check';
-%thePath = 'C:\Users\tmc54\Desktop\NARSAD\TextFiles29-Jan-2018';
-%thePath = 'C:\Users\tmc54\Desktop\ACT_T1\TextFiles09-May-2018';
 thePath = 'C:\Users\tmc54\Documents\MATLAB\TextFiles05-Jul-2018';
 cd(thePath)
 mainFolder = dir;
-listOfFiles = {mainFolder(3:end).name}; %change back to 3:end after testing
-%label={'ACT127_T1'};; %this will run just specific subjects if you have the
-%next line commented out
-label=listOfFiles; %this will run all subjects that you have in thePath
-alphaPeakThreshold = 80;
-maxInterpThreshold = 37;
-outputName = ['subjectsToCheck' date];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fid = fopen(outputName, 'w');
-k=size(label);
-j=k(2);
-for kk=1:j
+listOfFiles = {mainFolder(3:end).name}; 
+alphaPeakThreshold = 80; % Maximum power of the alpha peak we allow (in microvolts squared)
+maxInterpThreshold = 37; % How many bad electrodes are we allowing out of 124
+outputName = ['subjectsToCheck' date]; % name of the output file
+
+%% 
+
+fid = fopen(outputName, 'w'); % create a blank output file
+for fileNumber=1:size(listOfFiles,2)
     cd(thePath)
-    varname = label{kk};
-    if strcmp(varname(end), 's')
+    fileName = listOfFiles{fileNumber};
+    % There are two types of files in the path, maxAlphaFreqs and
+    % maxChanInterp, so the following if statements check to see which
+    % category the current file falls under.
+    if strcmp(fileName(11:end), 'maxAlphaFreqs') 
         try
-            maxAlphaFreqs = load(varname);
-                for n = 1:size(maxAlphaFreqs, 2)
-                    if maxAlphaFreqs(n) >= alphaPeakThreshold
-                        trigger = [varname(1:end-14) ' segment ' int2str(n) ' has a 5-10 Hz band peak above ' int2str(alphaPeakThreshold) ' microvolts (' int2str(maxAlphaFreqs(n)) ')'];
-                        fprintf(fid, [trigger '\n']);
+            maxAlphaFreqs = load(fileName); % load the max alpha file into a variable
+                for segmentNumber = 1:size(maxAlphaFreqs, 2) % go through each of the segments for the file (typically 6)
+                    if maxAlphaFreqs(segmentNumber) >= alphaPeakThreshold
+                        trigger = [fileName(1:9) ' segment ' int2str(segmentNumber) ' has a 5-10 Hz band peak above ' int2str(alphaPeakThreshold) ' microvolts (' int2str(maxAlphaFreqs(segmentNumber)) ')'];
+                        fprintf(fid, [trigger '\n']); % add the above line to the output file
                     end
-%                     if maxAlphaFreqs(n) ==0 && n<7
-%                         trigger = [varname(1:end-14) ' segment ' int2str(n) ' has a 0'];
-%                         fprintf(fid, [trigger '\n']);
-%                     end
                 end
         catch
         end
     end
-    if strcmp(varname(end), 'p')
+    if strcmp(fileName(11:end), 'maxChanInterp')
         try
-            maxInterpolatedChannels = load(varname);
-            for l = 1:size(maxInterpolatedChannels,2)
-                if maxInterpolatedChannels(l) >= maxInterpThreshold
-                    trigger = [varname(1:end-14) ' segment ' int2str(l) ' has ' int2str(maxInterpolatedChannels(l)) ' interpolated channels'];
-                    fprintf(fid, [trigger '\n']);
+            maxInterpolatedChannels = load(fileName); % load the interpolated channels file into a variable
+            for segmentNumber = 1:size(maxInterpolatedChannels,2)
+                if maxInterpolatedChannels(segmentNumber) >= maxInterpThreshold
+                    trigger = [fileName(1:9) ' segment ' int2str(segmentNumber) ' has ' int2str(maxInterpolatedChannels(segmentNumber)) ' interpolated channels'];
+                    fprintf(fid, [trigger '\n']); % add the above line to the output file
                 end
 
             end
@@ -51,4 +57,4 @@ for kk=1:j
 
 
 end
-fid = fclose(fid);
+fid = fclose(fid); % close the output file when we are done
--- checkExcelFiles.m
+++ checkExcelFiles.m
@@ -1,58 +1,75 @@
+% Script created in response to realizing that different eye tracking
+% computers were exporting files with different settings. This script is
+% meant to be run after all eye tracking data has been exported from the
+% Tobii computers at both sites. 
+
+% This script will check to make sure that:
+% Files from both sites are using the appropriate filters. 
+% There are all 3 Gaze Event Types (Fixation, Saccade, Unclassified). 
+% There are 21 unique segments for each subject.
+% There is only one subject in each Excel file. 
+
 clear
 thePath = 'C:\Users\tmc54\Desktop\Segmented2';
 cd(thePath)
 mainFolder = dir;
 listOfSubjectFolders = {mainFolder(3:end).name};
-label=listOfSubjectFolders; %this will run all subjects that you have in thePath
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-segmentNames={'DB1' 'DB2' 'DB3' 'DB4' 'DB5' 'DB6' 'DB7' 'DB8' 'DB9' 'DB10' 'DB11' 'JAmonkey' 'JAmoose' 'JArooster' 'JApenguin' 'sandwich1' 'sandwich2' 'MTmonkey' 'MTmoose' 'MTrooster' 'MTpenguin'};
-j=size(label,2);
-for folderNumber=1:j
+[wrongFilterType, subjectNamesDifferent, multipleSubjectsInOneFile, ...
+ missingGazeEventType, abnormalNumberOfUniqueSegments, wrongFinalSegment,...
+ wrongColumns] = deal(zeros(size(listOfSubjectFolders)));
+%%
+
+segmentNames={'DB1' 'DB2' 'DB3' 'DB4' 'DB5' 'DB6' 'DB7' 'DB8' 'DB9' 'DB10' 'DB11' 'JAmonkey' 'JAmoose' 'JArooster' 'JApenguin' 'sandwich1' 'sandwich2' 'MTmonkey' 'MTmoose' 'MTrooster' 'MTpenguin'}; % The list of names for each segment in the Sandwich Lady video
+for folderNumber=1:size(listOfSubjectFolders,2)
     cd(thePath)
-    varname = label{folderNumber};
-    filename=[ varname 'a.xls']
-    cd(varname);
-    [num, txt, raw]=xlsread(filename); 
-    raw = raw(2:end,:);
+    subjectName = listOfSubjectFolders{folderNumber};
+    filename=[ subjectName 'a.xls'];
+    cd(subjectName);
+    [~, ~, raw]=xlsread(filename); % load the subject's Excel file
+    headers = raw(1,:); % saving the headers as a seperate variable, used at the end of this script
+    raw = raw(2:end,:); % removing the headers
     
     %% Check what filter type was used during the export process
     
-    filterType = unique(raw(:,10));
+    filterType = unique(raw(:,10)); % pull out the filter Type column, there should only be 1 value
     if ~strcmp(filterType, 'I-VT filter')
-        wrongFilterType(folderNumber) = 1;
+        wrongFilterType(folderNumber) = 1; % if the filter used was not I-VT, flag this subject
     end
     
     %% Check that there is only one subject name in the Excel file and that the subject name in Excel matches the Excel file name
     
-    subjectNameFromExcel(folderNumber) = unique(raw(:,6));
-    if ~strcmp(subjectNameFromExcel(folderNumber), varname)
-        subjectNamesDifferent(folderNumber) = 1;
+    subjectNameFromExcel = unique(raw(:,6)); % pull out the subject name from the Excel file, there should only be 1 value
+    if ~strcmp(subjectNameFromExcel, subjectName)
+        subjectNamesDifferent(folderNumber) = 1; % if the subject name in the file is not the same as the subject name in the file name itself, flag this file
     end
     if size(subjectNameFromExcel,1) > 1
-        multipleSubjectsInOneFile(folderNumber) = 1;
+        multipleSubjectsInOneFile(folderNumber) = 1; % if there are multiple subject names in the file, flag this file
     end
     
     %% Check that there are Fixations, Saccades, and Unclassifieds
     
-    gazeEventTypes = unique(raw(:,44));
-    fixationCheck = find(ismember(gazeEventTypes, 'Fixation'));
-    saccadeCheck = find(ismember(gazeEventTypes, 'Saccade'));
-    unclassifiedCheck = find(ismember(gazeEventTypes, 'Unclassified'));
+    gazeEventTypes = unique(raw(:,44)); % pull out the gaze event type column
+    fixationCheck = find(ismember(gazeEventTypes, 'Fixation')); % check for Fixations
+    saccadeCheck = find(ismember(gazeEventTypes, 'Saccade')); % check for Saccades
+    unclassifiedCheck = find(ismember(gazeEventTypes, 'Unclassified')); % check for Unclassifieds 
     
     if isempty(fixationCheck) && isempty(saccadeCheck) && isempty(unclassifiedCheck)
         missingGazeEventType(folderNumber) = 1;
     end
     
     %% Check Number of Unique Segments
-    % *** Still need to check if there is another segment after segment 21
+    
     numberOfUniqueSegments = size(unique(cellstr(raw(:,16))),1);
-    if numberOfUniqueSegments ~= 21
+    if numberOfUniqueSegments ~= 21 % We expect 21 unique segments in a normal experiment
         abnormalNumberOfUniqueSegments(folderNumber) = 1;
     end
     
+    % If the last segment isn't segment 21, there is a problem
     if ~strcmp(raw(end,16),'21')
         wrongFinalSegment(folderNumber) = 1;
     end
+    
+    %% Check the order of the columns
     
     if ~isequal(txt(1, :), {'ExportDate','StudioVersionRec','StudioProjectName','StudioTestName','ParticipantName','RecordingName','RecordingDate','RecordingDuration','RecordingResolution','FixationFilter','MediaName','MediaPosX (ADCSpx)','MediaPosY (ADCSpx)','MediaWidth','MediaHeight','SegmentName','SegmentStart','SegmentEnd','SegmentDuration','SceneName','SceneSegmentStart','SceneSegmentEnd','SceneSegmentDuration','RecordingTimestamp','LocalTimeStamp','EyeTrackerTimestamp','MouseEventIndex','MouseEvent','MouseEventX (ADCSpx)','MouseEventY (ADCSpx)','MouseEventX (MCSpx)','MouseEventY (MCSpx)','KeyPressEventIndex','KeyPressEvent','StudioEventIndex','StudioEvent','StudioEventData','ExternalEventIndex','ExternalEvent','ExternalEventValue','EventMarkerValue','FixationIndex','SaccadeIndex','GazeEventType','GazeEventDuration','FixationPointX (MCSpx)','FixationPointY (MCSpx)','AOI[Penguin]Hit','AOI[Rooster]Hit','AOI[Moose]Hit','AOI[Monkey]Hit','AOI[Hands]Hit','AOI[Body]Hit','AOI[Eyes]Hit','AOI[Mouth]Hit','AOI[Background]Hit','GazePointIndex','GazePointLeftX (ADCSpx)','GazePointLeftY (ADCSpx)','GazePointRightX (ADCSpx)','GazePointRightY (ADCSpx)','GazePointX (ADCSpx)','GazePointY (ADCSpx)','GazePointX (MCSpx)','GazePointY (MCSpx)','GazePointLeftX (ADCSmm)','GazePointLeftY (ADCSmm)','GazePointRightX (ADCSmm)','GazePointRightY (ADCSmm)','StrictAverageGazePointX (ADCSmm)','StrictAverageGazePointY (ADCSmm)','EyePosLeftX (ADCSmm)','EyePosLeftY (ADCSmm)','EyePosLeftZ (ADCSmm)','EyePosRightX (ADCSmm)','EyePosRightY (ADCSmm)','EyePosRightZ (ADCSmm)','CamLeftX','CamLeftY','CamRightX','CamRightY','DistanceLeft','DistanceRight','PupilLeft','PupilRight','ValidityLeft','ValidityRight','IRMarkerCount','IRMarkerID','PupilGlassesRight'})
         wrongColumns(folderNumber) = 1;
