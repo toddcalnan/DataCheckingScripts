e9b279bc62b37f3c77b6b23b0f2843ed68f982b4
Todd Calnan <todd.calnan@duke.edu>
2019-11-26 09:29:18 -0500

    Updated comments and variable names to improve clarity.

--- RunLogToFolders.m
+++ RunLogToFolders.m
@@ -1,25 +1,21 @@
-%Moves all Run Logs that you have in thePath into the subject folders in
-%thePath. If there is no matching folder for the run log to go in, it just
-%won't move. Really quick, significantly better than moving by hand. Just
-%copy and paste run logs from Neuro_data into your directory and run this.
+% Moves all Run Logs that you have in thePath into the subject folders in
+% thePath. If there is no matching folder for the run log to go in, it just
+% won't move. Really quick, significantly better than moving by hand. Just
+% copy and paste run logs from Neuro_data into your directory and run this.
 
-clear all
-%thePath = 'C:\Users\tmc54\Documents';
+clear
 thePath = 'C:\Users\tmc54\Documents\EyeTracking\Segment3718';
 cd(thePath)
 mainFolder = dir;
 listOfSubjectFolders = {mainFolder(3:end).name};
-label=listOfSubjectFolders; %this will run all subjects that you have in thePath; it pleases me greatly
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-k=size(label);
-j=k(2);
-for kk=1:j
+for subjectNumber=1:size(listOfSubjectFolders,2)
     cd(thePath)  
-    varname = label{kk};
-    if size(varname,2) > 9
-        if any(strcmp(listOfSubjectFolders, varname(1:9)))
-            movefile(varname, varname(1:9))
+    subjectName = listOfSubjectFolders{subjectNumber};
+    if size(subjectName,2) > 9
+        if any(strcmp(listOfSubjectFolders, subjectName(1:9)))
+            movefile(subjectName, subjectName(1:9))
         end    
     end
 end
--- individualDB.m
+++ individualDB.m
@@ -1,25 +1,41 @@
+% Pulls out raw sample counts on total samples viewing media as well as 
+% total samples on each ROI for each subject. 
+% The path should contain all of the analyzed subject files outputted from
+% EGT_Analysis_Master.
+% Output will be an Excel file containing a large matrix of sample counts.
+% Rows will be the subjects
+% Columns will be total samples on media, followed by the 10 ROI counts.
+% These columns are repeated 10 more times, for a total of 11 sets, one per
+% DB condition
+
 clear
-thePath = 'C:\Users\tmc54\Documents\EyeTracking\DukeACT_EGT\T2Background';
+thePath = 'C:\Users\tmc54\Documents\EyeTracking\DukeACT\T2Background';
+outputFileName = 'rawSampleCountsSeperateDBsT2.xlsx'; % the name of the output file
 cd(thePath)
 mainFolder = dir;
 listOfSubjectFolders = {mainFolder(3:end).name};
-label=listOfSubjectFolders; %this will run all subjects that you have in thePath
-j=size(label,2);
-allSubjectsRawCounts = [];
+allSubjectsRawCounts = []; % preallocating
 
-for folderNumber=1
+for folderNumber=1:size(listOfSubjectFolders,2) % go through all subjects
     cd(thePath)
-    varname = label{folderNumber}(1:9);  %for DukeACT
-    filename=[ varname '_EGT_Analysis.mat'];
-    cd(varname);
-    analysisFile = load(filename, ['ACT_Data_' varname]);
-    datFile = analysisFile.(['ACT_Data_' varname]);
+    subjectName = listOfSubjectFolders{folderNumber}(1:9);  %for DukeACT
+    filename=[ subjectName '_EGT_Analysis.mat'];
+    cd(subjectName);
+    analysisFile = load(filename, ['ACT_Data_' subjectName]);
+    datFile = analysisFile.(['ACT_Data_' subjectName]);
     listOfSegmentNames = fieldnames(datFile);
-    subjectRawCounts = [];
-    for segmentNumber = 1:11 % 11 DB segments
-        dbSegment = datFile.(listOfSegmentNames{segmentNumber});
-        subjectRawCounts = [subjectRawCounts, dbSegment{2,[5,7:14,25]}];
+    subjectRawCounts = []; % preallocating
+    for segmentNumber = 1:11 % 11 DB segments, out of 21 total segments
+        dbSegment = datFile.(listOfSegmentNames{segmentNumber}); % pull the data for an individual DB segment
+        % Each dbSegment has 2 rows, the first are the headers, the second
+        % is the actual data
+        % Column 5 is the samples viewing media, while columns 7:14,16,25
+        % are the total samples in each ROI
+        subjectRawCounts = [subjectRawCounts, dbSegment{2,[5,7:14,16,25]}];
     end
-    
+    %% Merge all subjectRawCounts into a single matrix
     allSubjectsRawCounts = [allSubjectsRawCounts; subjectRawCounts];
 end
+
+%% Write output Excel file
+xlswrite(outputFileName, allSubjectsRawCounts);
